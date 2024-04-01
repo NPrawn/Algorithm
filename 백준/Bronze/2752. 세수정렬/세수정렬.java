97a2329359e1323bdb8a992bc69f52a565f53e17
NPrawn <163245782+NPrawn@users.noreply.github.com>
2024-04-01 16:53:43 +0900

    [Bronze IV] Title: 세수정렬, Time: 224 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -1,41 +1,16 @@
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int max, min, mid;
-        if (a < b && a < c) {
-            min = a;
-            if (b < c) {
-                mid = b;
-                max = c;
-            } else {
-                mid = c;
-                max = b;
-            }
-        } else if (b < a && b < c) {
-            min = b;
-            if (a < c) {
-                mid = a;
-                max = c;
-            } else {
-                mid = c;
-                max = a;
-            }
-        } else {
-            min = c;
-            if (a < b) {
-                mid = a;
-                max = b;
-            } else {
-                mid = b;
-                max = a;
-            }
+        int[] arr = new int[3];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println(min + " " + mid + " " + max);
+        Arrays.sort(arr);
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
     }
 }
