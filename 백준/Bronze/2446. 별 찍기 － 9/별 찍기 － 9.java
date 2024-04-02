b941210224e9c2b9201998e36d95e62f0a8046ce
NPrawn <163245782+NPrawn@users.noreply.github.com>
2024-04-02 15:32:16 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 140 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j < n; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0; j < i * 2 + 1; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        br.close();
+        bw.close();
+    }
+}
