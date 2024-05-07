804d00ee7e2f8b999b8ae6ef98ee4c6e485b7edf
NPrawn <163245782+NPrawn@users.noreply.github.com>
2024-05-07 14:36:12 +0900

    [Silver II] Title: N과 M (9), Time: 276 ms, Memory: 28660 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, arr[], ans[];
+    static boolean isUsed[];
+
+    static void func(int cur) {
+        if (cur == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(ans[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int tmp = 0;
+        for (int i = 0; i < n; i++) {
+            if (isUsed[i]) continue;
+            if (tmp == arr[i]) continue;
+            isUsed[i] = true;
+            ans[cur] = arr[i];
+            tmp = ans[cur];
+            func(cur + 1);
+            isUsed[i] = false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = toInt(st.nextToken());
+        m = toInt(st.nextToken());
+        arr = new int[n];
+        ans = new int[n];
+        isUsed = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = toInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        
+        func(0);
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+    static int toInt(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+}
