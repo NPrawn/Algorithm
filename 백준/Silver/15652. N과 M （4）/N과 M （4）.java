f40e546bdc16e568136b6fc0e37b4e0a62594a1d
NPrawn <163245782+NPrawn@users.noreply.github.com>
2024-05-07 12:45:37 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 19988 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, arr[];
+
+    static void func(int cur) {
+        if (cur == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int st = 1;
+        if (cur != 0) {
+            st = arr[cur - 1];
+        }
+        for (int i = st; i <= n; i++) {
+            arr[cur] = i;
+            func(cur + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = toInt(st.nextToken());
+        m = toInt(st.nextToken());
+        arr = new int[n + 1];
+
+        func(0);
+
+        bw.write(sb.toString());
+
+        bw.close();
+    }
+
+    public static int toInt(String s) {
+        return Integer.parseInt(s);
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+}
