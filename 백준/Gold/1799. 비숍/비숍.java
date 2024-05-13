6aae4c85b07a8a9107cea9a90c51dd055ad7fa07
NPrawn <163245782+NPrawn@users.noreply.github.com>
2024-05-13 14:42:06 +0900

    [Gold I] Title: 비숍, Time: 216 ms, Memory: 31772 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.java
+++ 백준/Gold/1799. 비숍/비숍.java
@@ -1,74 +1,68 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, board[][], k, arr[][], ans = 0, ct;
-    static boolean isUsed[];
+    static int n, ans[];
+    static ArrayList<Node> board[][];
+    static boolean isUsed[][];
 
-    static void func(int cur) {
-        if (cur == k) {
-            ans = Math.max(ans, ct);
-            return;
-        }
-        if (cur == 0) ct = 0;
-        int tmp = 0;
-        for (int i = 0; i < n; i++) {
-            int x, y;
-            if (cur >= n) {
-                x = n - 1 - i;
-                y = cur - x;
-            }else
-            {
-                y = i;
-                x = cur - y;
-            }
-            if (arr[cur][i] == 0) {
-                tmp++;
-                continue;
-            }
-            if (isUsed[n - 1 - x + y]) {
-                tmp++;
-                continue;
-            }
-            ct++;
-            isUsed[n - 1 - x + y] = true;
-            func(cur + 1);
-            isUsed[n - 1 - x + y] = false;
-            ct--;
-        }
-        if (tmp == n) {
-            func(cur + 1);
+
+    static class Node {
+        int x, y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        k = 2 * n - 1;
-        board = new int[n][n];
-        isUsed = new boolean[k];
-        arr = new int[k][n];
+    static void bishop(int idx, int cnt, int color) {
+        if (idx == 2 * n) {
+            ans[color] = Math.max(ans[color], cnt);
+            return;
+        }
+        for (var v : board[color][idx]) {
+            int x, y;
+            x = v.x;
+            y = v.y;
+            if (isUsed[color][x + y]) continue;
+            isUsed[color][x + y] = true;
+            bishop(idx + 1, cnt + 1, color);
+            isUsed[color][x + y] = false;
+        }
+        bishop(idx + 1, cnt, color);
+    }
 
+    public static void main(String[] args) throws IOException {
+        n = toInt(br.readLine());
+        board = new ArrayList[2][2 * n];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2 * n; j++) {
+                board[i][j] = new ArrayList<>();
+            }
+        }
+        ans = new int[2];
+        isUsed = new boolean[2][2 * n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if (i + j >= n) {
-                    arr[i + j][n - 1 - i] = board[i][j];
-                } else {
-                    arr[i + j][j] = board[i][j];
+                if (Integer.parseInt(st.nextToken()) == 1) {
+                    board[(i + j + 1) % 2][n - 1 - i + j].add(new Node(i, j));
                 }
             }
         }
 
-        func(0);
+        bishop(0, 0, 0);
+        bishop(0, 0, 1);
 
-
-        bw.write(ans + "");
-        br.close();
+        bw.write((ans[0] + ans[1]) + "");
         bw.close();
     }
 
+    static int toInt(String s) {
+        return Integer.parseInt(s);
+    }
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
