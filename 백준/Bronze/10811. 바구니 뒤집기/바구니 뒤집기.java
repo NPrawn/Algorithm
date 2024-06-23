2414926101651cdefa10c94863261f18d359c6ec
NPrawn <163245782+NPrawn@users.noreply.github.com>
2024-06-23 12:41:25 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 148 ms, Memory: 15896 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int arr[];
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            while (a < b) {
+                int tmp = arr[a];
+                arr[a++] = arr[b];
+                arr[b--] = tmp;
+            }
+        }
+
+        for (int e : arr) {
+            sb.append(e + " ");
+        }
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+}
