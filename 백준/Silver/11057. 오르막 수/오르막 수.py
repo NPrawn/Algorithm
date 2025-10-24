a74cca353e10b1ab6f786d2909381a9325b303c6
NPrawn <163245782+NPrawn@users.noreply.github.com>
2025-10-24 19:28:02 +0900

    [Silver I] Title: 오르막 수, Time: 108 ms, Memory: 111276 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,12 @@
+n = int(input())
+MOD = 10007
+
+dp = [[0 for _ in range(10)] for _ in range(n)]
+for i in range(10):
+    dp[0][i] = 1
+
+for i in range(1, n):
+    for j in range(10):
+        dp[i][j] = sum(dp[i-1][:j+1])%MOD
+
+print(sum(dp[n-1])%MOD)
