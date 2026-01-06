282d8808b77f5e9bb849d74b9d78d1a7dfd7a677
NPrawn <163245782+NPrawn@users.noreply.github.com>
2026-01-06 19:40:30 +0900

    [D2] Title: 두 개의 숫자열, Time: 57 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,15 +1,54 @@
-T = int(input())
-for tc in range(1, T+1):
-	n, m = map(int, input().split())
-	A = list(map(int, input().split()))
-	B = list(map(int, input().split()))
-	if n>m: A, B = B, A; n,m = m,n
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-	ans = -float('inf')
-	for i in range(m-n+1):
-		res = 0
-		for a,b in zip(A, B[i:i+n]):
-			res += a*b
-		ans = max(ans,res)
-	
-	print(f'#{tc} {ans}')
+public class Solution {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            int[] A = new int[n];
+            int[] B = new int[m];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                A[i] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < m; i++) {
+                B[i] = Integer.parseInt(st.nextToken());
+            }
+
+            if (m < n) {
+                int tmp = n;
+                n = m;
+                m = tmp;
+                int[] arr = A;
+                A = B;
+                B = arr;
+            }
+            int ans = Integer.MIN_VALUE;
+            for (int st = 0; st <= m - n; st++) {
+                int ct = 0;
+                for (int i = 0; i < n; i++) {
+                    ct += A[i] * B[st + i];
+                }
+                ans = Math.max(ct, ans);
+            }
+
+            sb.append("#").append(t).append(" ").append(ans).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
