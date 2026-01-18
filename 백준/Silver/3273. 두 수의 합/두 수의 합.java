8132fd6c157fe69249a3593ba927376d30f672b1
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-01-18 21:32:07 +0900

    [Silver III] Title: 두 수의 합, Time: 324 ms, Memory: 27228 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,39 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    static int n, x, ans = 0;
+    static int[] arr;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int ct = 0;
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int x = Integer.parseInt(br.readLine());
+        x = Integer.parseInt(br.readLine());
+
         Arrays.sort(arr);
-        int left = 0;
-        int right = n - 1;
-        int sum = 0;
-        while (left < right) {
-            sum = arr[left] + arr[right];
-            if (sum == x) {
-                left++;
-                right--;
-                ct++;
-            } else if (sum > x) {
-                right--;
+        int l = 0;
+        int r = n-1;
+        while (l < r) {
+            int a = arr[l];
+            int b = arr[r];
+            if (a + b == x) {
+                ans++;
+                l++;
+                r--;
+            } else if (a + b < x) {
+                l+=1;
             } else {
-                left++;
+                r-=1;
             }
         }
-        bw.write(Integer.toString(ct));
 
-        br.close();
-        bw.close();
+        sb.append(ans);
+        System.out.println(sb.toString());
     }
 }
