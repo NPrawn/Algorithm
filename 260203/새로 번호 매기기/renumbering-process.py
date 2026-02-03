b0a3d560c210b634311377cf4349fcd412a1f30e
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-02-03 23:11:36 +0900

    Title: 새로 번호 매기기, Time: 405ms, Memory: 36MB, Status: Passed - Codetree

--- 260203/새로 번호 매기기/renumbering-process.py
+++ 260203/새로 번호 매기기/renumbering-process.py
@@ -7,25 +7,24 @@ deg = [0 for _ in range(n+1)]
 q = []
 for _ in range(m):
     x, y = map(int, input().split())
-    adj[x].append(y)
-    deg[y] += 1
+    adj[y].append(x)
+    deg[x] += 1
 
 for i in range(1, n+1):
-    if deg[i] == 0: heapq.heappush(q, i)
+    if deg[i] == 0: heapq.heappush(q, -i)
 
-ans = []
+ct = n
+ans = [0 for _ in range(n+1)]
 while q:
-    now = heapq.heappop(q)
-    ans.append(now)
+    now = -heapq.heappop(q)
+    ans[now] = ct
+    ct-= 1
     for a in adj[now]:
         deg[a] -= 1
         if deg[a] == 0:
-            heapq.heappush(q, a)
+            heapq.heappush(q, -a)
 
-res = [0 for _ in range(n+1)]
-if (len(ans) == n):
-    for i,a in enumerate(ans, start = 1):
-        res[a] = i
-    print(*res[1:])
+if sum(ans) == sum(i for i in range(n+1)) :
+    print(*ans[1:])
 else:
     print(-1)
