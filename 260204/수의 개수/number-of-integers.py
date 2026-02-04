46bf8369ecadeabcc9fd23cc37de119d55c4398c
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-02-04 14:17:53 +0900

    Title: 수의 개수, Time: 249ms, Memory: 22MB, Status: Passed - Codetree

--- 260204/수의 개수/number-of-integers.py
+++ 260204/수의 개수/number-of-integers.py
@@ -1,1 +1,28 @@
 n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+def lower_bound(t):
+    l, r = 0, n
+
+    while l < r:
+        mid = (l+r) // 2
+        if arr[mid] < x:
+            l = mid + 1
+        else:
+            r = mid
+    return l
+
+def upper_bound(t):
+    l, r = 0, n
+
+    while l < r:
+        mid = (l+r) // 2
+        if arr[mid] <= x:
+            l = mid + 1
+        else:
+            r = mid
+    return l
+
+for _ in range(m):
+    x = int(input())
+    print(upper_bound(x) - lower_bound(x))
