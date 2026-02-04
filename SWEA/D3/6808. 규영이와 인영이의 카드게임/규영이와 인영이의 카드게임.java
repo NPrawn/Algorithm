031bf71fdf5d54f842d16552da0587119003a86f
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-02-04 16:31:13 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 5,106 ms, Memory: 26,496 KB -BaekjoonHub

---
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	static int win = 0;
+	static int lose = 0;
+	static int n = 9;
+	static boolean[] used = new boolean[19];
+	static int[] A = new int[n];
+	static int[] B = new int[n];
+
+	public static void main(String[] args) throws Exception {
+		int T = Integer.parseInt(input.readLine());
+		for (int t=1; t<=T; t++) {
+			used = new boolean[19];
+			win = lose = 0;
+			used[0] = true;
+			st = new StringTokenizer(input.readLine());
+			for (int i =0;i<n;i++) {
+				A[i] = Integer.parseInt(st.nextToken());
+				used[A[i]] = true;
+			}
+			
+			dfs(0);
+			
+			sb.append("#").append(t).append(" ").append(win).append(" ").append(lose).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	static void dfs(int depth) {
+		if (depth == n) {
+			int a = 0;
+			int b = 0;
+			for (int i=0; i<n;i++) {
+				if (A[i] > B[i]) a += A[i]+B[i];
+				else b+= A[i]+B[i];
+			}
+			if (a > b) win+=1;
+			else lose+=1;
+		}
+		
+		for(int k=1; k<19; k++) {
+			if (used[k]) continue;
+			B[depth] = k;
+			used[k] = true;
+			dfs(depth + 1);
+			used[k] = false;
+		}
+	}
+}
