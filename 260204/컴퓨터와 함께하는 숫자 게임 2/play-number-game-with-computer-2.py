16085a9cf8ff0d0535c146147750ffa9c0f8df25
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-02-04 16:50:39 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260204/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
+++ 260204/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
@@ -2,14 +2,14 @@ m = int(input())
 a, b = map(int, input().split())
 
 def lower_bound(x):
-    l, r = 0, m+1
+    l, r = 1, m
     ct = 0
-    while l<r:
+    while l<=r:
         ct+=1
         mid = (l+r) // 2
         if mid == x: return ct
         elif mid < x: l = mid + 1
-        else: r = mid
+        else: r = mid - 1
     return ct
 
 mn = float('inf')
