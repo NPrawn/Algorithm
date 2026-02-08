19536ca58c645eb7d20869fa7efcf36d0a79445b
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-02-08 10:28:19 +0900

    [Gold V] Title: 두 용액, Time: 324 ms, Memory: 28148 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,87 +1,41 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	static int n;
-	static int[] arr;
 
-	public static void main(String[] args) throws Exception {
-		n = Integer.parseInt(input.readLine());
-		arr = new int[n];
-		st = new StringTokenizer(input.readLine());
-		for (int i = 0; i < n; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-		Arrays.sort(arr);
-		int best = Integer.MAX_VALUE;
-		int a = 0, b = 0;
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(input.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(input.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		for (int i = 0; i < n; i++) {
-			int x = arr[i];
-			int idx = lowerBound(-x);
+        Arrays.sort(arr);
+        int l=0;
+        int r = n - 1;
+        int[] ans = new int[2];
+        int min = Integer.MAX_VALUE;
 
-			if (idx < n && idx != i) {
-				int y = arr[idx];
-				int sum = x + y;
-				int abs = Math.abs(sum);
-				if (abs < best) {
-					best = abs;
-					a = x;
-					b = y;
-				}
-			}
+        while (l < r) {
+            int res = arr[l] + arr[r];
+            if (Math.abs(res) < min) {
+                min = Math.abs(res);
+                ans[0] = arr[l];
+                ans[1] = arr[r];
+            }
 
-			if (idx - 1 >= 0 && idx - 1 != i) {
-				int y = arr[idx - 1];
-				int sum = x + y;
-				int abs = Math.abs(sum);
-				if (abs < best) {
-					best = abs;
-					a = x;
-					b = y;
-				}
-			}
-		}
+            if (res < 0) {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
 
-		if (a > b) {
-			int tmp = a;
-			a = b;
-			b = tmp;
-		}
-		System.out.println(a + " " + b);
-
-	}
-
-	public static int lowerBound(int target) {
-		int lo = 0;
-		int hi = n;
-		while (lo < hi) {
-			int mid = lo + (hi - lo) / 2;
-
-			if (arr[mid] >= target)
-				hi = mid;
-			else
-				lo = mid + 1;
-		}
-
-		return lo;
-	}
-
-	public static int upperBound(int target) {
-		int lo = 0;
-		int hi = n;
-
-		while (lo < hi) {
-			int mid = lo + (hi - lo) / 2;
-			if (arr[mid] > target)
-				hi = mid;
-			else
-				lo = mid + 1;
-		}
-
-		return lo;
-	}
+        System.out.println(ans[0] + " " + ans[1]);
+    }
 }
