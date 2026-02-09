324d616f18b983d02c14ca3b0dbdb4e01d457958
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-02-09 14:52:53 +0900

    Title: 다른 괄호로 이동하기, Time: 249ms, Memory: 23MB, Status: Wrong Answer - Codetree

---
+++ 260209/다른 괄호로 이동하기/move-to-another-parenthesis.py
@@ -0,0 +1,32 @@
+import heapq
+
+n, a, b = map(int, input().split())
+inf = float('inf')
+grid = []
+dxdy = [(1,0), (0,1), (-1,0), (0,-1)]
+for _ in range(n):
+    grid.append(list(input()))
+
+for i in range(n):
+    for j in range(n):
+        dist = [[inf for _ in range(n)] for _ in range(n)]
+        dist[i][j] = 0
+        q = []
+        heapq.heappush(q, (0,i,j))
+        while q:
+            w, x, y = heapq.heappop(q)
+            for dx,dy in dxdy:
+                nx, ny = x+dx, y+dy
+                if not (0<=nx<n and 0<=ny<n): continue
+                nw = a if grid[x][y] == grid[nx][ny] else b
+                if dist[nx][ny] <= dist[x][y] + nw: continue
+                dist[nx][ny] = dist[x][y] + nw
+                heapq.heappush(q, (dist[nx][ny], nx, ny))
+        
+        mx = 0
+        for rows in dist:
+            for item in rows:
+                if item == inf: continue
+                mx = max(mx, item)
+        
+print(mx)
