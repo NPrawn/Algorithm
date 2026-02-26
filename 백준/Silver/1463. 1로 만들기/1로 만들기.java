57fe8590dd30ce91d4925d9aa173d16d8b5c11ef
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-02-26 15:43:49 +0900

    [Silver III] Title: 1로 만들기, Time: 168 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,29 +1,44 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb = new StringBuilder();
-    static int[] D;
-
-    public static void main(String[] args) throws IOException {
-        int X = Integer.parseInt(br.readLine());
-        D = new int[X + 1];
-        D[1] = 0;
-
-        for (int i = 2; i <= X; i++) {
-            D[i] = D[i - 1] + 1;
-            if (i % 2 == 0) {
-                D[i] = Math.min(D[i], D[i / 2] + 1);
-            }
-            if (i % 3 == 0) {
-                D[i] = Math.min(D[i], D[i / 3] + 1);
-            }
-        }
-
-        sb.append(D[X]);
-        bw.write(sb.toString());
-        bw.close();
-    }
-
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(input.readLine());
+		int[] arr = new int[n+1];
+		arr[n] = 1;
+		
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		q.add(n);
+		while(!q.isEmpty()) {
+			int now = q.pollFirst();
+			int nxt = 0;
+			if (now%3==0) {
+				nxt = now/3;
+				if (arr[nxt] == 0) {
+					arr[nxt] = arr[now]+1;
+					q.add(nxt);
+				}
+			}
+			if (now%2==0) {
+				nxt = now/2;
+				if (arr[nxt]==0) {
+					arr[nxt] = arr[now]+1;
+					q.add(nxt);
+				}
+			}
+			nxt = now - 1;
+			if (nxt >= 0) {
+				if (arr[nxt]==0) {
+					arr[nxt] = arr[now]+1;
+					q.add(nxt);
+				}
+			}
+		}
+		
+		System.out.println(arr[1]-1);
+	}
 }
