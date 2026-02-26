0568acbce2e26185f465de63da0a3b5ac909756c
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-02-26 09:40:24 +0900

    [Gold V] Title: 토마토, Time: 616 ms, Memory: 107680 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,74 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static class Pair {
-        public int x;
-        public int y;
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
 
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        int[][] board = new int[n][m];
-        int[][] dist = new int[n][m];
-        Queue<Pair> que = new LinkedList<>();
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                dist[i][j] = 0;
-                if (board[i][j] == 1) {
-                    que.add(new Pair(i, j));
-                }
-                if (board[i][j] == 0) {
-                    dist[i][j] = -1;
-                }
-            }
-        }
-        int[] dx = new int[]{1, 0, -1, 0};
-        int[] dy = new int[]{0, 1, 0, -1};
-
-        while (!que.isEmpty()) {
-            Pair cur = que.poll();
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                    continue;
-                }
-                if (board[nx][ny] != 0 || dist[nx][ny] >= 0) {
-                    continue;
-                }
-                dist[nx][ny] = dist[cur.x][cur.y] + 1;
-                que.add(new Pair(nx, ny));
-            }
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (dist[i][j] == -1) {
-                    bw.write("-1");
-                    bw.flush();
-                    System.exit(0);
-                }
-                ans = Math.max(dist[i][j], ans);
-            }
-        }
-        bw.write(Integer.toString(ans));
-
-
-        br.close();
-        bw.close();
-    }
+	public static void main(String[] args) throws Exception {
+		int[] dx = {1,0,-1,0};
+		int[] dy = {0,1,0,-1};
+		
+		st = new StringTokenizer(input.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int[][] grid = new int[n][m];
+		int[][] dist = new int[n][m];
+		int ans = 0;
+		ArrayDeque<int[]> dq = new ArrayDeque<>();
+		
+		for (int x=0;x<n;x++) {
+			st = new StringTokenizer(input.readLine());
+			for (int y=0;y<m;y++) {
+				grid[x][y] = Integer.parseInt(st.nextToken());
+				if (grid[x][y] == 1) {
+					dq.add(new int[] {x,y});
+					dist[x][y] = 1;
+				}
+			}
+		}
+		
+		while (!dq.isEmpty()) {
+			int[] now = dq.pollFirst();
+			int x = now[0];
+			int y = now[1];
+			
+			for (int d=0; d<4;d++) {
+				int nx = x+dx[d];
+				int ny = y+dy[d];
+				if (nx<0||ny<0||nx>=n||ny>=m) continue;
+				if (dist[nx][ny] != 0) continue;
+				if (grid[nx][ny] == -1) continue;
+				dist[nx][ny] = dist[x][y] + 1;
+				dq.add(new int[] {nx,ny});
+			}
+		}
+		
+		for (int x=0; x<n; x++) {
+			for (int y=0; y<m; y++) {
+				ans = Math.max(ans, dist[x][y]-1);
+				if (grid[x][y] == 0 && dist[x][y] == 0) {
+					ans = -1;
+					break;
+				}
+			}
+			
+			if (ans == -1) break;
+		}
+		
+		System.out.println(ans);
+	}
 }
