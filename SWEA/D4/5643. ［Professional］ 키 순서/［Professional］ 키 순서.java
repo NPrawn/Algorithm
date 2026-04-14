933288b80a56a0e043b86e4b9caa202216a8d329
JeongWoo Lee <163245782+NPrawn@users.noreply.github.com>
2026-04-14 12:05:40 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,886 ms, Memory: 101,376 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -7,65 +7,40 @@ class Solution {
     static StringTokenizer st;
 
     public static void main(String[] args) throws Exception {
-        int T = Integer.parseInt(input.readLine());
 
+        int T = Integer.parseInt(input.readLine());
         for (int tc=1; tc<=T; tc++) {
-    
             int n = Integer.parseInt(input.readLine());
             int m = Integer.parseInt(input.readLine());
-            ArrayList<Integer>[] adj1 = new ArrayList[n+1];
-            ArrayList<Integer>[] adj2 = new ArrayList[n+1];
-            int ans = 0;
-            for (int i=0; i<=n; i++){
-                adj1[i] = new ArrayList<>();
-                adj2[i] = new ArrayList<>();
-            }
-            for (int i=0;i<m; i++) {
+
+            boolean[][] ck = new boolean[n+1][n+1];
+
+            for (int t = 0; t < m; t++) {
                 st = new StringTokenizer(input.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                adj1[a].add(b);
-                adj2[b].add(a);
+                ck[a][b] = true;
             }
-    
-            for (int i=1; i<n+1;i++) {
-                int now = i;
-                boolean[] vis = new boolean[n+1];
-                vis[now] = true;
-                int ct = 0;
-                ArrayDeque<Integer> dq = new ArrayDeque<>();
-                dq.add(now);
-                while (!dq.isEmpty()) {
-                    now = dq.pollFirst();
-                    for (int nxt : adj1[now]) {
-                        if (vis[nxt]) continue;
-                        ct += 1;
-                        dq.add(nxt);
-                        vis[nxt] = true;
+
+            for (int k = 1; k <= n; k++) {
+                for (int i = 1; i <= n; i++) {
+                    for (int j = 1; j <= n; j++) {
+                        if (ck[i][k] && ck[k][j]) ck[i][j] = true;
                     }
                 }
-                now = i;
-                vis = new boolean[n+1];
-                vis[now] = true;
-                dq = new ArrayDeque<>();
-                dq.add(now);
-    
-                while (!dq.isEmpty()) {
-                    now = dq.pollFirst();
-                    for (int nxt : adj2[now]) {
-                        if (vis[nxt]) continue;
-                        ct += 1;
-                        dq.add(nxt);
-                        vis[nxt] = true;
-                    }
-                }
-    
-                if (ct == n-1) ans += 1;
-                
             }
-    
-            sb.append("#").append(tc).append(" ").append(ans).append("\n");
+            int ans = 0;
+            for (int i = 1; i <= n; i++) {
+                int cnt = 0;
+                for (int j = 1; j <= n; j++) {
+                    if (i == j) continue;
+                    if (ck[i][j] || ck[j][i]) cnt++;
+                }
+                if (cnt == n - 1) ans++;
+            }
+            sb.append("#").append(tc).append(" ").append(ans).append('\n');
         }
+
         System.out.println(sb.toString());
     }
 
